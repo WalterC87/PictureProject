532b04dfdcbbcb579d36d6a458e5cf5bc8e38b79
Walter Cordero <wdlara.8789@gmail.com>
2014-03-07 17:39:15 -0600

    initial commit Printoo

--- app/router.js
+++ app/router.js
@@ -4,6 +4,14 @@ var routerController = function(app){
 	app.get('/', function (req,res){
 		res.render('index');
 	})
+
+	app.get('/registro/usuario', function (req,res){
+		res.render('usuario');
+	})
+
+	app.get('/registro/empresa', function (req,res){
+		res.render('empresa');
+	})
 }
 
 module.exports = routerController;
